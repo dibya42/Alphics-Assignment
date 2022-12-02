8f607df00690f73d68df12c03032e543247c1a19
dibya42 <dibyakanti095@gmail.com>
2022-12-02 19:00:03 +0530

    new

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'facebookAuth':{
+        'clientID':'852007272611823',
+        'clientSecret':'c0d8573b2b951d43720a95dd65d36572',
+        'callbackURL':'http://localhost:3000/auth/facebook/callback'
+    }
+}
